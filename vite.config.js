d2ff05227011a4246eb5395021f2e83545102dd3
caorui <778943319@qq.com>
2025-06-20 14:53:34 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,33 @@
+/*
+ * @Author: caorui 778943319@qq.com
+ * @Date: 2025-06-20 14:43:34
+ * @LastEditors: caorui 778943319@qq.com
+ * @LastEditTime: 2025-06-20 14:50:57
+ * @FilePath: \CesiumSDK_Examples\vite.config.js
+ * @Description: 
+ * 
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    }),
   ],
   resolve: {
     alias: {
